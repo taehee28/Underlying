43547e79862b9c014d081b0c0cec656c54c8660d
taehee28 <dev.thk28@gmail.com>
2024-01-13 15:46:13 +0900

    RESOURCE: Typography 정의 및 정리

--- app/src/main/java/com/thk/underlying/ui/theme/Type.kt
+++ app/src/main/java/com/thk/underlying/ui/theme/Type.kt
@@ -1,23 +1,15 @@
 package com.thk.underlying.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.sp
 import com.thk.underlying.R
-
-val UnderlyingFont = FontFamily(
-    Font(
-        resId = R.font.pretendard,
-        weight = FontWeight.Normal
-    ),
-    Font(
-        resId = R.font.pretendard,
-        weight = FontWeight.SemiBold
-    )
-)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -44,12 +36,18 @@ val Typography = Typography(
         fontFamily = UnderlyingFont,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
-    ),
-    labelMedium = TextStyle(
-        color = Gray950,
-        fontFamily = UnderlyingFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
     )
 )
+
+val Typography.textButtonLarge: TextStyle
+    @Composable
+    get() = UnderlyingTypography.textButtonLarge
+
+val Typography.textButtonSmall: TextStyle
+    @Composable
+    get() = UnderlyingTypography.textButtonSmall
+
+val Typography.titleLargeSemiBold: TextStyle
+    @Composable
+    get() = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.SemiBold)
 
---
+++ app/src/main/java/com/thk/underlying/ui/theme/UnderlyingTypography.kt
@@ -0,0 +1,37 @@
+package com.thk.underlying.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
+import com.thk.underlying.R
+
+val UnderlyingFont = FontFamily(
+    Font(
+        resId = R.font.pretendard,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.pretendard,
+        weight = FontWeight.SemiBold
+    )
+)
+
+object UnderlyingTypography {
+    val textButtonLarge: TextStyle = TextStyle(
+        color = Gray950,
+        fontFamily = UnderlyingFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        textDecoration = TextDecoration.Underline
+    )
+    val textButtonSmall: TextStyle = TextStyle(
+        color = Gray950,
+        fontFamily = UnderlyingFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        textDecoration = TextDecoration.Underline
+    )
+}
