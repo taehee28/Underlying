a838fd3945c5ab11e6e4692880b864f29097860b
Kang Taehee <dev.thk28@gmail.com>
2024-01-15 16:10:50 +0900

    FEAT: Modifier.noRippleClickable 정의

--- app/src/main/java/com/thk/underlying/ui/components/ComposeUtil.kt
+++ app/src/main/java/com/thk/underlying/ui/components/ComposeUtil.kt
@@ -1,6 +1,11 @@
 package com.thk.underlying.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
@@ -8,3 +13,17 @@ import androidx.compose.ui.unit.sp
 val Int.fixedSp: TextUnit
     @Composable
     get() = (this / LocalDensity.current.fontScale).sp
+
+fun Modifier.noRippleClickable(
+    enabled: Boolean = true,
+    onClick: () -> Unit
+): Modifier = composed {
+    this.clickable(
+        onClick = onClick,
+        enabled = enabled,
+        interactionSource = remember {
+            MutableInteractionSource()
+        },
+        indication = null
+    )
+}
