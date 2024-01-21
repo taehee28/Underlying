4d6feda222dfcfe259bc463c8c3a450c3e32fafb
taehee28 <dev.thk28@gmail.com>
2024-01-21 20:19:58 +0900

    viewModel-compose 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 }
